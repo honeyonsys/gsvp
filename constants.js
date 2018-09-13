ecd3ca86329aef2d35fa2016d78129433da909fc
harishsmtech <harish@sandeepani.in>
2018-09-13 22:24:38 +0530

    init

---
+++ constants.js
@@ -0,0 +1,7 @@
+const app = {
+    gApiKey: 'AIzaSyC4-IqfaeSLI5d36H86WLhZUyHH2acyLQU',
+    gClientId: '744856426448-smuhvljvjt5t0i6dbrlge8nmn6rrv7s1.apps.googleusercontent.com',
+    gClientSecret: 'ZMt0yCNYEyrEgwBVO-WuwrjH',
+    // appUrl: 'http://honeyonsys.github.io/apps/streetviewpublish/'
+    appUrl: 'http://localhost/honeyonsys.github.io/apps/streetviewpublish/'
+};
