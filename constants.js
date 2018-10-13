4989c491576042d1b7ee051a3dca572e776b75ea
honeyonsys <honeyonsys@gmail.com>
2018-10-13 13:16:41 +0530

    adding google auth

--- constants.js
+++ constants.js
@@ -2,6 +2,7 @@ const app = {
     gApiKey: 'AIzaSyC4-IqfaeSLI5d36H86WLhZUyHH2acyLQU',
     gClientId: '744856426448-smuhvljvjt5t0i6dbrlge8nmn6rrv7s1.apps.googleusercontent.com',
     gClientSecret: 'ZMt0yCNYEyrEgwBVO-WuwrjH',
-    // appUrl: 'http://honeyonsys.github.io/apps/streetviewpublish/'
-    appUrl: 'http://localhost/honeyonsys.github.io/apps/streetviewpublish/'
+    gEmail: 'honeyonsys@gmail.com',
+    // appUrl: 'http://honeyonsys.github.io/gsvp/'
+    appUrl: 'http://localhost/gsvp/'
 };
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 jQuery(document).ready(function(){
     // alert('bhak '+app.gApiKey);
-    
+    $('#google-signin-scope').attr('content', app.gEmail)
+    $('#google-signin-client_id').attr('content', app.gClientId)    
 })
