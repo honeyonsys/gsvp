3201676af8bfdd8ddb226c42719237fcd02b8cb1
honeyonsys <honeyonsys@gmail.com>
2018-10-14 15:28:21 +0530

    main page redirection for OAuth

--- constants.js
+++ constants.js
@@ -2,7 +2,9 @@ const app = {
     gApiKey: 'AIzaSyC4-IqfaeSLI5d36H86WLhZUyHH2acyLQU',
     gClientId: '744856426448-smuhvljvjt5t0i6dbrlge8nmn6rrv7s1.apps.googleusercontent.com',
     gClientSecret: 'ZMt0yCNYEyrEgwBVO-WuwrjH',
+    gAuthURL: 'https://accounts.google.com/o/oauth2/auth',
+    streetViewScope: 'https://www.googleapis.com/auth/streetviewpublish',
     gEmail: 'honeyonsys@gmail.com',
-    // appUrl: 'http://honeyonsys.github.io/gsvp/'
-    appUrl: 'http://localhost/gsvp/'
+    appUrl: 'http://honeyonsys.github.io/gsvp/'
+    // appUrl: 'http://localhost/gsvp/'
 };
